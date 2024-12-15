b8767620c33b90b4f1ad4c8879803bd1da836907
LiHua-Java-Poet <1550217434@qq.com>
2024-12-15 21:50:14 +0800

    初始化项目

---
+++ minzi-plan/src/main/java/com/example/plan/MinziPlanApplication.java
@@ -0,0 +1,13 @@
+package com.example.plan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinziPlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinziPlanApplication.class, args);
+    }
+
+}
